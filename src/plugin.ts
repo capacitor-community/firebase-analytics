2ecd2fe80a3a82c4f2ef9bbce87a383ba4da54a3
Stewan <oi@stewan.io>
2019-07-05 19:14:04 -0300

    fix(plugin): api

--- src/plugin.ts
+++ src/plugin.ts
@@ -10,21 +10,34 @@ import {
 const { AnalyticsPlugin } = Plugins;
 
 export class Analytics implements AnalyticsProtocol {
+  enable(): Promise<void> {
+    return AnalyticsPlugin.enable();
+  }
+
+  disable(): Promise<void> {
+    return AnalyticsPlugin.disable();
+  }
+
   instance(): Promise<{ id: string }> {
     return AnalyticsPlugin.instance();
   }
+
   reset(): Promise<void> {
     return AnalyticsPlugin.reset();
   }
+
   setScreen(options: ScreenParams): Promise<void> {
     return AnalyticsPlugin.setScreen(options);
   }
+
   setUserID(options: UserIDParams): Promise<void> {
     return AnalyticsPlugin.setUserID(options);
   }
+
   setUserProp(options: UserPropParams): Promise<void> {
     return AnalyticsPlugin.setUserProp(options);
   }
+
   logEvent(options: LogEventParams): Promise<void> {
     return AnalyticsPlugin.logEvent(options);
   }
