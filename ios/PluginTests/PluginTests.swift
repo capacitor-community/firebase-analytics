0eeae5273c23fc9990a5c0c2f10e311779b8f1c0
Chris Abernethy <brownoxford@gmail.com>
2021-04-09 15:54:07 -0400

    test: clean up plugin tests to match product and plugin name

--- ios/PluginTests/PluginTests.swift
+++ ios/PluginTests/PluginTests.swift
@@ -1,26 +1,26 @@
 import XCTest
 import Capacitor
-@testable import Plugin
+@testable import FBAnalytics
 
 class PluginTests: XCTestCase {
-    
+
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-    
+
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
+
     func testEcho() {
         // This is an example of a functional test case for a plugin.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        
+
         let value = "Hello, World!"
-        let plugin = MyPlugin()
-        
+        let plugin = FirebaseAnalytics()
+
         let call = CAPPluginCall(callbackId: "test", options: [
             "value": value
         ], success: { (result, call) in
@@ -29,7 +29,7 @@ class PluginTests: XCTestCase {
         }, error: { (err) in
             XCTFail("Error shouldn't have been called")
         })
-        
+
         plugin.echo(call!)
     }
 }
