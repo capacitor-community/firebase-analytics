e3c059b831a841062dca658ae11b53419eecf897
ptmkenny <1451472+ptmkenny@users.noreply.github.com>
2021-05-23 03:47:11 +0900

    databaseUrl should not be required in config (#73)
    * databaseUrl is not required
    
    * README: update feature table to match currently supported features

--- src/definitions.ts
+++ src/definitions.ts
@@ -24,7 +24,7 @@ export interface FirebaseAnalyticsPlugin {
 export interface FirebaseInitOptions {
   apiKey: string;
   authDomain: string;
-  databaseURL: string;
+  databaseURL?: string;
   projectId: string;
   storageBucket: string;
   messagingSenderId: string;
