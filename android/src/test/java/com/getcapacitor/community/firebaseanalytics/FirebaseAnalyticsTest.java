f32bd57ba320a62d8567851acf6221d5b9570fb7
Wyatt Ades <wyattades@gmail.com>
2021-10-01 01:01:47 -0700

    add test comments

--- android/src/test/java/com/getcapacitor/community/firebaseanalytics/FirebaseAnalyticsTest.java
+++ android/src/test/java/com/getcapacitor/community/firebaseanalytics/FirebaseAnalyticsTest.java
@@ -8,10 +8,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-import androidx.annotation.Nullable;
-
 import java.util.Arrays;
-import java.util.stream.Collectors;
 
 public class FirebaseAnalyticsTest {
   
@@ -19,27 +16,41 @@ public class FirebaseAnalyticsTest {
     return FirebaseAnalytics.convertJsonToBundle(new JSONObject(json));
   }
 
-  private void check(String json, String bundleStr) throws Exception {
+  private void assertBundle(String json, String bundleStr) throws Exception {
     assertEquals(bundleStr, toBundle(json).toString());
   }
 
   @Test
   public void convertJsonToBundle() throws Exception {
-    check("{}", "Bundle[{}]");
-    check("{ \"hello\": \"world\" }", "Bundle[{hello=world}]");
-    check("{ \"foo\": 123.456 }", "Bundle[{foo=123.456}]");
-    check("{ \"foo\": null }", "Bundle[{}]");
-    check("{ \"foo\": { \"bar\": 123 } }", "Bundle[{foo=Bundle[{bar=123}]}]");
-    check("{ \"foo\": {} }", "Bundle[{foo=Bundle[{}]}]");
-    
+    // basic fields
+    assertBundle("{}", "Bundle[{}]");
+    assertBundle("{ \"hello\": \"world\" }", "Bundle[{hello=world}]");
+    assertBundle("{ \"foo\": 123.456 }", "Bundle[{foo=123.456}]");
+    assertBundle("{ \"foo\": null }", "Bundle[{}]");
+
+    // nested objects
+    assertBundle("{ \"foo\": { \"bar\": 123 } }", "Bundle[{foo=Bundle[{bar=123}]}]");
+    assertBundle("{ \"foo\": {} }", "Bundle[{foo=Bundle[{}]}]");
+
+    // don't allow mismatched array elements
+    assertBundle("{ \"foo\": [{ \"bar\": 123 }, 456.789], \"fizz\": 123 }", "Bundle[{fizz=123}]");
+
+    // array of bundles
     assertArrayEquals(
       new String[]{"Bundle[{bar=123}]", "Bundle[{fizz=456}]"},
-      Arrays.stream(toBundle("{ \"foo\": [{ \"bar\": 123 }, { \"fizz\": 456 }] }").getParcelableArray("foo")).map(p -> p.toString()).collect(Collectors.toList()).toArray()
+      Arrays.stream(toBundle("{ \"foo\": [{ \"bar\": 123 }, { \"fizz\": 456 }] }").getParcelableArray("foo")).map(p -> p.toString()).toArray()
     );
 
-    assertArrayEquals(new float[]{123.0f, 456.0f}, toBundle("{ \"foo\": [123, 456] }").getFloatArray("foo"), 0.0f);
-    assertArrayEquals(new float[]{123.0f, 456.789f}, toBundle("{ \"foo\": [123, 456.789] }").getFloatArray("foo"), 0.0f);
-
-    check("{ \"foo\": [{ \"bar\": 123 }, 456.789], \"fizz\": 123 }", "Bundle[{fizz=123}]");
+    // any combination of number types in array should be fine
+    assertArrayEquals(
+      new float[]{123, 456},
+      toBundle("{ \"foo\": [123, 456] }").getFloatArray("foo"),
+      0
+    );
+    assertArrayEquals(
+      new float[]{123, 456.789f},
+      toBundle("{ \"foo\": [123, 456.789] }").getFloatArray("foo"),
+      0
+    );
   }
 }
