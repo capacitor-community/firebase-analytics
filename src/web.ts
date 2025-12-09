cdce92af848a9a0ee0dcb7d2d8eb25772858061e
tom.vanhowe <tom.vanhowe@prestomusic.com>
2025-12-09 15:38:03 +0000

    Load firebase app script before analytics script on web

--- src/web.ts
+++ src/web.ts
@@ -294,10 +294,12 @@ export class FirebaseAnalyticsWeb
 
       resolve(
         Promise.all([
-          this.loadScript(firebaseAppScript.key, firebaseAppScript.src),
-          this.loadScript(
-            firebaseAnalyticsScript.key,
-            firebaseAnalyticsScript.src,
+          this.loadScript(firebaseAppScript.key, firebaseAppScript.src).then(
+            () =>
+              this.loadScript(
+                firebaseAnalyticsScript.key,
+                firebaseAnalyticsScript.src,
+              ),
           ),
         ]),
       );
